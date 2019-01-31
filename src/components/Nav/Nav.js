21b30c0b2e62c718081f64a05ce3b7677f01bbfb
Jay Sargent <sargentjay11@gmail.com>
2019-01-31 06:57:19 -0700

    initial commit

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './nav.css'
+
+const Nav = () => {
+    return (
+        <nav>
+            <p>Page One</p>
+            <p>Page Two</p>
+            <p>Page Three</p>
+        </nav>
+    )
+}
+export default Nav
